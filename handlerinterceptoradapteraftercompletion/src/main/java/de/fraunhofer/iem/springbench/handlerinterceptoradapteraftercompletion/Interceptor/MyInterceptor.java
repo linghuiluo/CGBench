344249aceb99a4babe8f3df8631a19a51df66975
Ranjith K <ranjith.masthikatte@gmail.com>
2020-09-16 21:22:25 +0200

    Changed HandlerInterceptor into HandlerInterceptorAdapter

--- handlerinterceptoradapteraftercompletion/src/main/java/de/fraunhofer/iem/springbench/handlerinterceptoradapteraftercompletion/Interceptor/MyInterceptor.java
+++ handlerinterceptoradapteraftercompletion/src/main/java/de/fraunhofer/iem/springbench/handlerinterceptoradapteraftercompletion/Interceptor/MyInterceptor.java
@@ -1,12 +1,12 @@
 package de.fraunhofer.iem.springbench.handlerinterceptoradapteraftercompletion.Interceptor;
 
 import de.fraunhofer.iem.springbench.handlerinterceptoradapteraftercompletion.LOGGER.MyLogger;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyInterceptor implements HandlerInterceptor {
+public class MyInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String user = request.getParameter("user");
