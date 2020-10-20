518a105923571b0d59ffd8314599d1163b635acb
Ranjith K <ranjith.masthikatte@gmail.com>
2020-10-20 23:41:05 +0200

    Updated initbinderwithoutvalue

--- initbinderwithoutvalue/src/main/java/de/fraunhofer/iem/springbench/initbinderwithoutvalue/controllers/MyController.java
+++ initbinderwithoutvalue/src/main/java/de/fraunhofer/iem/springbench/initbinderwithoutvalue/controllers/MyController.java
@@ -8,44 +8,40 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class MyController {
     @InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
-        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(" ", true);
-        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    public void initBinder(HttpServletRequest request,
+                           HttpServletResponse response,
+                           WebDataBinder dataBinder) {
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        String user = request.getParameter("user");
+
+        if (user.contains(" ")) {
+            try {
+                response.getWriter().append("Trimming username: " + user + ".........\n\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(" ", true);
+            dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+        }
     }
 
     @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     public void index(@RequestParam("user") String user, HttpServletResponse response) throws IOException {
-        String responseHTML = createHTML(user);
 
-        response.setContentType("text/html;charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        //Method append is a sink, responseHTML will be displayed on client's browser. If there is a script then it will be
-        //executed on client's browser.
-        response.getWriter().append(responseHTML);
-    }
+        // Authenticate the user and continue with the process,
+        // Here, we will consider that authentication fails and response with Invalid username
 
-    //This method creates a response HTML page.
-    private static String createHTML(String name) {
-
-        return "<html>\r\n" +
-                "	<head>\r\n" +
-                "		<meta charset=\"UTF-8\">\r\n" +
-                "		<title>Hello World</title>\r\n" +
-                "	</head>\r\n" +
-                "	<body>\r\n" +
-                "		<script>\r\n" +
-                "			var c = \"secret=this is secret cookie;\" \r\n" +
-                "			document.cookie = c;\r\n" +
-                "		</script>\r\n" + "		<pre>\r\n" +
-                "			Hello " + name + "!! Welcome to myWebsite\r\n." +
-                "		</pre>\r\n" +
-                "	</body>\r\n" +
-                "</html>";
+        response.getWriter().append("\n\nInvalid User Name.");
     }
 }
